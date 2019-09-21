a3499637b4dc08e871ebc6fc987ed9742ecc95cd
MYLS <wiryls@gmx.com>
2019-09-21 19:12:56 +0800

    rename qerrors to wrap

---
+++ errors/wrap/quick_errors.go
@@ -0,0 +1,64 @@
+package wrap
+
+import (
+	"fmt"
+
+	"github.com/wiryls/pkg/errors/detail"
+)
+
+// Message wraps an error with message, alias.
+//  - Return nil if `inner` is nil.
+//  - It uses `fmt.Errorf(msg+": %w", inner)` to create an error.
+func Message(inner error, msg string) error {
+	if inner == nil {
+		return nil
+	}
+
+	return fmt.Errorf(msg+": %w", inner)
+}
+
+// MessageStack wraps an error with message, alias.
+//  - Return nil if `inner` is nil.
+//  - It creates a quick and simple detail.Detail error.
+func MessageStack(inner error, msg string, skipCaller uint) error {
+	if inner == nil {
+		return nil
+	}
+
+	return detail.New(
+		msg,
+		detail.FlagInner(inner),
+		detail.FlagStackTrace(skipCaller+1),
+	)
+}
+
+// MessageAlias wraps an error with message, alias.
+//  - Return nil if `inner` is nil.
+//  - It creates a quick and simple detail.Detail error.
+func MessageAlias(inner error, msg string, alias error) error {
+	if inner == nil {
+		return nil
+	}
+
+	return detail.New(
+		msg,
+		detail.FlagInner(inner),
+		detail.FlagAlias(alias),
+	)
+}
+
+// MessageAliasStack wraps an error with message, alias and stacktrace.
+//  - Return nil if `inner` is nil.
+//  - It creates a quick and simple detail.Detail error.
+func MessageAliasStack(inner error, msg string, alias error, skipCaller uint) error {
+	if inner == nil {
+		return nil
+	}
+
+	return detail.New(
+		msg,
+		detail.FlagInner(inner),
+		detail.FlagAlias(alias),
+		detail.FlagStackTrace(skipCaller+1),
+	)
+}
